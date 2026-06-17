7c15ee8269e2fc74d721ce5fc859bcc2e9b8220e
Jaka Kordez <jaka.kordez@gmail.com>
2026-06-17 17:54:39 +0200

    Improved album deletion dialog

--- app/src/main/java/si/pocketalbum/view/AlbumsPanel.kt
+++ app/src/main/java/si/pocketalbum/view/AlbumsPanel.kt
@@ -4,20 +4,24 @@ package si.pocketalbum.view
 
 import android.app.AlertDialog
 import android.content.Context
-import android.content.DialogInterface
 import android.content.Intent
+import android.graphics.Color
 import android.util.AttributeSet
+import android.view.LayoutInflater
 import android.view.MotionEvent
 import android.view.View
 import android.widget.AdapterView
+import android.widget.Button
+import android.widget.EditText
 import android.widget.FrameLayout
 import android.widget.ImageButton
 import android.widget.ListView
+import android.widget.TextView
+import androidx.core.graphics.drawable.toDrawable
 import si.pocketalbum.ImportActivity
 import si.pocketalbum.R
 import si.pocketalbum.StatisticsActivity
 import si.pocketalbum.core.AlbumConnection
-import si.pocketalbum.core.AlbumIndex
 import si.pocketalbum.services.AlbumService
 import kotlin.uuid.ExperimentalUuidApi
 
@@ -66,7 +70,11 @@ class AlbumsPanel (ctx: Context, attrs: AttributeSet?) : FrameLayout(ctx, attrs)
 
                 if (position < adapter.count - 1)
                 {
-                    showDeleteDialog(adapter.getItem(position), service)
+                    val album = adapter.getItem(position)
+                    showDeleteAlbumDialog(album.name)
+                    {
+                        service.deleteAlbum(album)
+                    }
                     return true
                 }
                 return false
@@ -74,24 +82,36 @@ class AlbumsPanel (ctx: Context, attrs: AttributeSet?) : FrameLayout(ctx, attrs)
         }
     }
 
-    fun showDeleteDialog(album: AlbumIndex.AlbumLocator, service: AlbumService)
-    {
-        val dialogClickListener: DialogInterface.OnClickListener =
-            object : DialogInterface.OnClickListener {
-                override fun onClick(dialog: DialogInterface?, which: Int) {
-                    if (which == DialogInterface.BUTTON_POSITIVE)
-                    {
-                        service.deleteAlbum(album)
-                    }
-                }
-            }
+    fun showDeleteAlbumDialog(
+        albumName: String,
+        onDeleteConfirmed: () -> Unit
+    ) {
+        val view = LayoutInflater.from(context)
+            .inflate(R.layout.dialog_delete_album, null)
 
-        val builder = AlertDialog.Builder(context)
-        builder.setMessage("Do you wish to delete album '${album.name}' from application storage? " +
-                "This operation can not be reversed.")
-            .setPositiveButton("Yes", dialogClickListener)
-            .setNegativeButton("No", dialogClickListener)
-            .show()
+        val txtAlbumName = view.findViewById<TextView>(R.id.txtAlbumName)
+        val edtConfirmation = view.findViewById<EditText>(R.id.edtConfirmation)
+        val btnCancel = view.findViewById<Button>(R.id.btnCancel)
+        val btnDelete = view.findViewById<Button>(R.id.btnDelete)
+
+        txtAlbumName.text = albumName
+
+        val dialog = AlertDialog.Builder(context)
+            .setView(view)
+            .create()
+
+        btnDelete.setOnClickListener {
+            if (edtConfirmation.text.toString().trim() == albumName) {
+                dialog.dismiss()
+                onDeleteConfirmed()
+            }
+        }
+
+        btnCancel.setOnClickListener {
+            dialog.dismiss()
+        }
+        dialog.show()
+        dialog.window?.setBackgroundDrawable(Color.TRANSPARENT.toDrawable())
     }
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
